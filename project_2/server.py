aba54e243abd85d14309cc74319bc420670f27a6
brandicast <brandicast@gmail.com>
2024-08-06 00:40:59 +0800

    trim the response

--- project_2/server.py
+++ project_2/server.py
@@ -1,36 +1,35 @@
-from flask import Flask, request 
+import time
+import gemini.agent as AI
+from flask import Flask, request
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-import gemini.agent as AI
 
-import time
- 
-app = Flask(__name__) 
- 
-@app.route('/') 
-def home(): 
-    return  "Hello"
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Hello"
+
 
 @app.route('/chat/id/<user>')
 def askWithLineID(user):
     ask = request.args.get('ask')
 
-    answer = AI.ask (user, ask) 
+    answer = AI.ask(user, ask)
 
+    return (str(answer).strip())
 
-    return (str(answer))
 
 @app.route('/chat/history/<user>')
 def historyWithLineID(user):
-    history = AI.history (user)
+    history = AI.history(user)
     return (history)
 
 
-
-
-if __name__ == '__main__': 
+if __name__ == '__main__':
     AI.loadChatSession()
-    app.run(host='0.0.0.0') 
+    app.run(host='0.0.0.0')
