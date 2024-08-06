6cafdaba06dcb16339e103fb977c683c57bf695a
Brandon Wu <brandicast@gmail.com>
2024-08-06 19:17:42 +0800

    move the pre tag from model to flash

--- project_2/server.py
+++ project_2/server.py
@@ -1,35 +1,36 @@
-import time
-import gemini.agent as AI
-from flask import Flask, request
+from flask import Flask, request 
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+import gemini.agent as AI
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return "Hello"
-
+import time
+ 
+app = Flask(__name__) 
+ 
+@app.route('/') 
+def home(): 
+    return  "Hello"
 
 @app.route('/chat/id/<user>')
 def askWithLineID(user):
     ask = request.args.get('ask')
 
-    answer = AI.ask(user, ask)
+    answer = AI.ask (user, ask) 
+
 
     return (str(answer).strip())
 
-
 @app.route('/chat/history/<user>')
 def historyWithLineID(user):
-    history = AI.history(user)
-    return (history)
+    history = AI.history (user)
+    return ('<pre>' + history + '</pre>')
 
 
-if __name__ == '__main__':
+
+
+if __name__ == '__main__': 
     AI.loadChatSession()
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0') 
